28925fe445162b197dd5689a2639185f9b97a29f
ekayaint <ekayaint@gmail.com>
2023-07-31 21:41:31 +0300

    Initial Commit

---
+++ SwiftUI-Speedometer-Sample/SwiftUI_Speedometer_SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Speedometer_SampleApp.swift
+//  SwiftUI-Speedometer-Sample
+//
+//  Created by ekayaint on 31.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Speedometer_SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
